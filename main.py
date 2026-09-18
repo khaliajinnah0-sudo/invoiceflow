3ea139311958ccea08504131f619643f1803a77b
Khalia Jinnah <khaliajinnah0@gmail.com>
2026-09-18 09:42:07 +0300

    Create styled InvoiceFlow dashboard

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Welcome to InvoiceFlow"}
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={}
+    )
